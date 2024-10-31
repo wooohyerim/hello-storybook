91a8621422493a1a5483b4906c701b611e5ebdd0
wooohyerim <hyerim3414@gmail.com>
2024-10-31 14:36:00 +0900

    font 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 function App() {
-  return <div className="text-3xl font-bold bg-red-300">test</div>
+  return <div className="text-3xl font-bold bg-pink-200 text-mono300">test</div>
 }
 
 export default App
